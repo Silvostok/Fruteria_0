0a1dbc8d8f43715b6f401d1f5f5ee9da50af7071
Silvostok <testing repo>
2015-08-12 14:39:51 +0200

    Initial Commit

---
+++ Fruteria/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fruteria
+//
+//  Created by Miguel Angel Rubio Caballero on 12/08/15.
+//  Copyright © 2015 MiguelRubio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
